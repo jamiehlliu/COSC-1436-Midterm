a1aa324de862e705ed5b07523110ce24ce9c4c56
jamiehlliu <jamiehlliu@gmail.com>
2026-07-02 10:54:39 -0500

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,17 @@
+package midtermpackage;
+
+import java.util.Scanner;
+
+public class Question1 {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many oz of milk the child drank? ");
+		double milk = input.nextDouble();
+		
+		if (milk >= 6) {
+			System.out.println("Baby will sleep well for 5 hours.");
+		} else {
+			System.out.println("Baby will not sleep well. So be prepared.");
+		}
+	}
+}
